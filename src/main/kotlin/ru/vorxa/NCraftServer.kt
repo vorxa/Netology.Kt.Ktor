084858b25ea68b8cf9084b8c1afd9c0c67b4b965
vorxa <vorxa@yandex.ru>
2019-11-28 12:06:20 +0700

    init

---
+++ src/main/kotlin/ru/vorxa/NCraftServer.kt
@@ -0,0 +1,12 @@
+package ru.vorxa
+
+import io.ktor.application.Application
+import io.ktor.server.cio.EngineMain
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.module() {
+    
+}
